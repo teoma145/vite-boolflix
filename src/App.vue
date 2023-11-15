f273cffd332ef6b102af974c3052356359c0395c
teoma145 <matteodark145@yahoo.it>
2023-11-15 15:24:54 +0100

    start

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+      <h1>NPM RUN</h1>
+    </div>
+</template>
+
+<script>
+import axios from 'axios';
+import {store} from './data';
+export default {
+    data () {
+        
+
+        return {}
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
